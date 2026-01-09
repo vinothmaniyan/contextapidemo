9ef8cd20aabd9625e9048c9f7b6e1456fe44d6b0
unknown <vinothmaniyan6263@gmail.com>
2026-01-09 10:30:36 +0530

    update name

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,7 @@ function Profile(){
     const {name}=useContext(LoginContext)
     return(
         <div>
-            <h2>This is Profile page</h2>
+            <h2>This is Profile Page</h2>
             <h2>Welcome {name}</h2>
         </div>
     )
